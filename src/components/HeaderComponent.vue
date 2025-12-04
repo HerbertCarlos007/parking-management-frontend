acb46eec8feee0cff6855ca8f0c81ed794a9fbb2
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2025-12-03 21:12:12 -0300

    feat: criando header s idebar

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+   <div class="w-full bg-secondary border-2 border-border">
+        <div class="text-white flex justify-between p-6 items-center h-full">
+            <section><h2 class="text-2xl">ParkControl</h2></section>
+            <section>Admin</section>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/SidebarComponent.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="w-64 min-h-screen bg-secondary text-white p-4 border-r-2 border-border">
+        <h3>Menu</h3>
+        <div>
+            <ul>
+                <li class="py-2 hover:bg-green-500 rounded px-2 cursor-pointer">Dashboard</li>
+                <li class="py-2 hover:bg-green-500 rounded px-2 cursor-pointer">Entrada/Saída</li>
+                <li class="py-2 hover:bg-green-500 rounded px-2 cursor-pointer">Gerenciar Vagas</li>
+                <li class="py-2 hover:bg-green-500 rounded px-2 cursor-pointer">Funcionários</li>
+                <li class="py-2 hover:bg-green-500 rounded px-2 cursor-pointer">Clientes e Veículos</li>
+                <li class="py-2 hover:bg-green-500 rounded px-2 cursor-pointer">Transações</li>
+                <li class="py-2 hover:bg-green-500 rounded px-2 cursor-pointer">Relatórios</li>
+                <li class="py-2 hover:bg-green-500 rounded px-2 cursor-pointer">Configurações</li>
+            </ul>    
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
